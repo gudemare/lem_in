d09292c5b94aa0de9fffcbe5066214744426593c
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-02-21 00:11:41 +0100

    Small comments.

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 00:06:46 by gudemare          #+#    #+#             */
-/*   Updated: 2018/02/20 22:22:43 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/02/21 00:11:21 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@
 ** putarr_fd
 ** Graph data structure (see wikipedia for functions)
 ** strip_whitespaces
+** double-linked lists
 */
 
 #ifndef LIBFT_H
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,16 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 18:41:28 by gudemare          #+#    #+#             */
-/*   Updated: 2018/02/20 23:28:01 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/02/21 00:05:09 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+/*
+** Properly freeing the whole graph will be a bit of a bitch.
+** It's lists in struts in lists, after all.
+*/
 
 static void	empty_anthill(t_anthill *anthill)
 {
