727245363d691d4b30f8fb5301e903ddd11ae8e8
Guillaume DEMAREZ <gudemare@student.42.fr>
2018-02-20 22:36:40 +0100

    Better error checking on get_ant_nb(). Precised supported format for input.

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,11 +6,23 @@
 /*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/20 20:46:43 by gudemare          #+#    #+#             */
-/*   Updated: 2018/02/20 21:26:48 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/02/20 22:35:48 by gudemare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+/*
+** Parsing does not support whitespaces differences.
+** Format supported is strictly :
+** "[nb]" for first line
+** "[name] [nb] [nb]" for nodes
+** "[name]-[name]" for edges
+** "##*" for commands.
+** "#*" for comments.
+** A ft_strip_whitespaces may get bonus points for supported formats ?
+** Probably not worth it.
+*/
 
 static void		get_ant_nb(t_anthill *anthill)
 {
@@ -18,24 +30,40 @@ static void		get_ant_nb(t_anthill *anthill)
 	int		i;
 
 	if (get_next_line(0, &str, 64) != 1)
-		ft_exit("Cannot read input.", 1, 1);
-	while (ft_is_whitespace(*str))
-		str++;
-	i = 1;
-	if (!*str || (*str != '+' && !ft_isdigit(*str)))
-		ft_exit("Bad ant number formating.", 0, 1);
+		ft_exit("Error : Cannot read input.", 1, 1);
+	if (!*str)
+	{
+		ft_strdel(&str);
+		ft_exit("Error : No ant number.", 0, 1);
+	}
+	i = (*str == '+');
 	while (str[i])
 		if (!ft_isdigit(str[i++]))
-			ft_exit("Bad ant number formating.", 0, 1);
-	if ((anthill->ant_nb = ft_atoi(str)) <= 0)
-		ft_exit("Bad ant number.", 0, 1);
-	if (!(anthill->entry_file = ft_strjoin(str, "\n")))
-		ft_exit("Insufficient memory.", 1, 1);
+		{
+			ft_strdel(&str);
+			ft_exit("Error : Bad ant number formating.", 0, 1);
+		}
+	anthill->ant_nb = ft_atoi(str);
+	anthill->entry_file = ft_strjoin(str, "\n");
+	ft_strdel(&str);
+	if (anthill->ant_nb <= 0)
+		ft_exit("Error : Bad ant number.", 0, 1);
+	if (!anthill->entry_file)
+		ft_exit("Error : Insufficient memory.", 1, 1);
 }
 
 static void		get_nodes(t_anthill *anthill)
 {
+	char	*str;
+	int		ret;
+
+	ret = get_next_line(0, &str, 64);
+	while (ret == 1)
+	{
+		ret = get_next_line(0, &str, 64);
+	}
 	(void)anthill;
+	ft_strdel(&str);
 }
 
 t_anthill		*fill_anthill(void)
@@ -43,7 +71,7 @@ t_anthill		*fill_anthill(void)
 	t_anthill	*anthill;
 
 	if (!(anthill = ft_memalloc(sizeof(t_anthill))))
-		ft_exit("Insufficient memory.", 1, 1);
+		ft_exit("Error : Insufficient memory.", 1, 1);
 	get_ant_nb(anthill);
 	get_nodes(anthill);
 	return (anthill);
