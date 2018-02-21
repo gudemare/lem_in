83fa435fb4a9f48d8951299e23b52489d31a8e0f
Mathieu GONON <mgonon@student.42.fr>
2018-02-21 20:47:56 +0100

    apply weight + find shortest path

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lem_in.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gudemare <gudemare@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgonon <mgonon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 04:52:57 by gudemare          #+#    #+#             */
-/*   Updated: 2018/02/21 17:13:28 by gudemare         ###   ########.fr       */
+/*   Updated: 2018/02/21 19:07:48 by mgonon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,24 @@
 **			linked to this node.
 */
 
-typedef struct		s_anthill
-{
-	char			*entry_file;
-	int				ant_nb;
-	char			*start_node;
-	char			*end_node;
-	t_list			*nodes;
-}					t_anthill;
-
 typedef struct		s_node
 {
-	char			*node;
+	char			*node_name;
+	int				weight;
 	int				coord_y;
 	int				coord_x;
 	t_list			*links;
 }					t_node;
+
+typedef struct		s_anthill
+{
+	char			*entry_file;
+	int				ant_nb;
+	t_node			*start_node;
+	t_node			*end_node;
+	t_list			*nodes;
+	t_list			*shortest_path;
+}					t_anthill;
 
 enum				e_line_type
 {
